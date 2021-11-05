42b72ff307ea51a36f790bc6dd1ad3c843ad339f
Benjamin Iduwe <bencoderus@gmail.com>
2021-11-05 11:18:47 +0100

    Block the event loop 😒

--- app.js
+++ app.js
@@ -4,4 +4,14 @@ const port = 3000;
 
 app.get('/', (req, res) => res.json('Hello World!'));
 
+app.get('/heavy', (req, res) => {
+  let counter = 0;
+
+  while (counter < 10000000000) {
+    counter++;
+  }
+
+  res.json(`Hello World! ${counter}`);
+});
+
 app.listen(port, () => console.log(`App listening on port ${port}!`));
